1ad0f637ed0f903012c2ed90663233ba7069865f
Nguyen Tien Dung <dungfopba2@gmail.com>
2023-10-31 17:12:36 +0700

    update new SlugPhone and Maintenance

---
+++ Controllers/SlugPhoneController.cs
@@ -0,0 +1,39 @@
+﻿using ReMoBi_DCSN.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReMoBi_DCSN.Controllers
+{
+    public class SlugPhoneController : Controller
+    {
+        public static SQLiteHandler db = SQLiteHandler.Instance;
+
+        private readonly dbDataContext dbdata;
+        public SlugPhoneController()
+        {
+            var connectionString = "Data Source=Albert;Database=news1;Trusted_Connection=True";
+            dbdata = new dbDataContext(connectionString);
+        }
+        // GET: Slug_Phone_
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult New13()
+        {
+            return View();
+        }
+        public ActionResult New11()
+        {
+            return View();
+        }
+        public ActionResult New14()
+        {
+            return View();
+        }
+    }
+}
